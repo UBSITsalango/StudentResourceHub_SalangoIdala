cd038e5193f47abed21401382494a7b860c90ab2
UBSIT-Idala <20233614@s.ubaguio.edu>
2025-04-03 14:49:17 +0800

    creating the footer layout and content

--- StudentResourceHub/src/app/footer/footer.component.ts
+++ StudentResourceHub/src/app/footer/footer.component.ts
@@ -2,10 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  template: `
+    <footer class="footer">
+      <p>&copy; {{ currentYear }} Student Resource Hub</p>
+      <p>Team Member 1: Idala, Ashley </p>
+      <p>Team Member 2: Salango, Tyrone</p>
+
+      </footer>
+  `,
+  styles: [`
+    .footer {
+      width: 100%;
+      text-align: center;
+      padding: 10px;
+      position: fixed;
+      bottom: 0;
+      background-color: #333;
+      color: white;
+    }
+  `]
 })
 export class FooterComponent {
-
+  currentYear: number = new Date().getFullYear();
 }
