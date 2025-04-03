31240171f916117b5e843ab205e271a56677c001
UBSITsalango <20162396@s.ubaguio.edu>
2025-04-03 18:08:46 +0800

    Fixing the header design.

--- StudentResourceHub/src/app/app.routes.ts
+++ StudentResourceHub/src/app/app.routes.ts
@@ -1,23 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CategoriesComponent } from './categories/categories.component';
 import { AboutComponent } from './about/about.component';
-import { AddResourceComponent } from './add-resource/add-resource.component';
+import { CategoriesComponent } from './categories/categories.component';
 import { ProgrammingComponent } from './categories/programming/programming.component';
 import { DesignComponent } from './categories/design/design.component';
 import { MathComponent } from './categories/math/math.component';
+import { AddResourceComponent } from './add-resource/add-resource.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { 
-    path: 'categories', 
-    component: CategoriesComponent,
-    children: [
+  { path: 'about', component: AboutComponent },
+  { path: 'categories', component: CategoriesComponent, children: [
       { path: 'programming', component: ProgrammingComponent },
       { path: 'design', component: DesignComponent },
       { path: 'math', component: MathComponent }
-    ]
+    ] 
   },
-  { path: 'about', component: AboutComponent },
   { path: 'add-resource', component: AddResourceComponent }
 ];
