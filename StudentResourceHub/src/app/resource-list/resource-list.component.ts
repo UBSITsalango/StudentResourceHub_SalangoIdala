e25175e12acfcd63f0db8b0232c5b1aee3978ea8
UBSITSalango <20162396@s.ubaguio.edu>
2025-04-03 14:38:01 +0800

    Creating the content of the resource-list component.

--- StudentResourceHub/src/app/resource-list/resource-list.component.ts
+++ StudentResourceHub/src/app/resource-list/resource-list.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Resource {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+}
 
 @Component({
   selector: 'app-resource-list',
-  imports: [],
   templateUrl: './resource-list.component.html',
-  styleUrl: './resource-list.component.css'
+  styleUrls: ['./resource-list.component.css']
 })
-export class ResourceListComponent {
+export class ResourceListComponent implements OnInit {
+  resources: Resource[] = [
+    { id: 1, title: 'Angular Basics', description: 'Introduction to Angular', category: 'Programming' },
+    { id: 2, title: 'UI/UX Design Principles', description: 'Understanding UI/UX', category: 'Design' },
+    { id: 3, title: 'Calculus Guide', description: 'Math resources for beginners', category: 'Math' }
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
